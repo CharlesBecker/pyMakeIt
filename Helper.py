9f50de2c25395958d6511e4c36749e74f1d94f06
Charles Becker <charleshbecker@gmail.com>
2014-12-05 07:45:14 -0700

    Created Helper.py module
    initial version

---
+++ Helper.py
@@ -0,0 +1,103 @@
+#Helper module
+
+import os
+
+class Helper(object):
+
+	def __init__(self, file_name, file_ext = "cpp", override = False):
+		"""Set up everything for the project and file creation"""
+		self.full_filename = file_name + "." + file_ext
+		self.project = file_name
+		self.project_path = os.getcwd() + "/" + file_name
+		self.precompile = {}
+		self.encoding = "utf-8"
+		self.file_name = file_name
+		self.file_ext = file_ext
+		self.override = override
+		self.standard = "#include <iostream>\n\n"
+		if (file_ext == "h"):
+			self.is_header = True
+		else:
+			self.is_header = False
+        
+
+	def doesExist(self, target = None):
+		"""	Checks to see if a file or directory exists
+			Postcondition:
+			Returns True if it does, False if it dosen't. """
+	
+		if (target == None):
+			target = self.project
+		
+		if (os.path.exists(target)):
+			return True
+		else:
+			return False
+			
+	def makeProject(self):
+		"""	Creates the directory for the project
+			Postcondition: 
+			If the directory existed then the program quits.
+			If the directory does not exist then it is created.
+			If self.override is True then the directory is created regardless. """
+			
+		if((self.doesExist(self.file_name) == False) or (self.override == True)):
+			os.mkdir(self.file_name)
+		else:
+			quit("That project already exists.")
+	
+	def getPrecompile(self):
+		"""	Creates the Precompiler definitions if this is a header file
+			Postcondition:
+			self.precompile dictionary is populated. """
+		
+		self.precompile["ifndef"] = "#ifndef H_{0}".format(self.file_name.upper())
+		self.precompile["define"] = "#define H_{0}".format(self.file_name.upper())
+		self.precompile["end"] = "#endif"
+		
+	def writeFile(self):
+		"""	Writes the file
+			Postcondition:
+			Correct file is created. """
+		os.chdir(self.project_path)
+		if(self.doesExist((self.full_filename) == False) or (self.override == True)):
+			fout = open(self.full_filename, "wb")
+			if (self.is_header == True):
+				self.writeHeader(self.precompile, fout)
+			else:
+				self.writeCPP(fout)
+				
+			if (self.is_header == True):
+				self.writeFooter(self.precompile, fout)
+			fout.close()
+		else:
+			quit("That file already exists.")
+
+
+	def writeCPP(self, out_file):
+		"""	Writes a standard barebones C++ file for you to use.
+			Postcondition:
+			C++ (cpp) file is created in the project directory. """
+		
+		out_file.write(bytes(self.standard, self.encoding))
+		out_file.write(bytes("int main()\n", self.encoding))
+		out_file.write(bytes("{\n\n", self.encoding))
+		out_file.write(bytes("}", self.encoding))
+					
+	def writeHeader(self, out_file):
+		""" If the file is a header file this outputs the precompiler opening directives.
+			Postcondition:
+			File has header file precompiler opening directives written to it. """
+		out_file.write(bytes(self.precompile['ifndef'], self.encoding))
+		out_file.write(bytes("\n", self.encoding))
+		out_file.write(bytes(self.precompile['define'], self.encoding))
+		out_file.write(bytes("\n", self.encoding))
+		out_file.write(bytes(self.standard, self.encoding))
+		
+	def writeFooter(self, out_file):
+		""" If the file is a header file this outputs the precompiler closing directives.
+			Postcondition:
+			File has header file precompiler closing directives written to it. """
+		out_file.write(bytes(self.precompile['end'], self.encoding))
+
+
