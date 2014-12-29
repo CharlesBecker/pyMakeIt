07b0c7af9a435695a62c5db19730bf0b60afc600
Charles Becker <charleshbecker@gmail.com>
2014-12-29 04:49:05 -0700

    Update makeIt.py
    Version 1.0 beta of makeIt.py, now functions from the command line.
    Note: for usage read readme file.
    Note: if filename given does not have a .h extension than it will be created as if it were a .cpp file
    Note: lots of area for improvement/refactoring.  Also needs to be more pythonic.
    Note: add comments.

--- makeIt.py
+++ makeIt.py
@@ -1,20 +1,69 @@
-#!/usr/local/bin/python3 
-# change the #! to the proper version for your system
-# also make the file executable
-
+#!/usr/local/bin/python3
 import getopt
 import sys
 import Helper
 
-project_name = "firstThing"
-file = "thirdThing"
-ext = "cpp"
-file_name = {'filename': file, 'ext': ext}
+def print_help():
+	print("This will be the help info")
+
+def makeit(value):
+	results = dict()
+	results['filename'], results['ext'] = value.split(".")
+	return results
+
+def create(value):
+	if (value['project'] is None):
+		value['project'] = value['filename']
+	project_name = value['project']
+	file_name = {'filename': value['filename'], 'ext': value['ext']}
+	override = value['override']
+	helper = Helper.Helper(project_name, file_name, override)
+
+	helper.getPrecompile()
+	helper.makeProject()
+	helper.writeFile()
+
+def main(value, control):
+	do_it = False
+	print("Here")
+	for opt, arg in options:
+		if opt in ('-o'):
+			if (arg.lower() == "t"):
+				control['override'] = True
+			print("Override is: " + str(control['override']))
+		elif opt in ('--Verbose'):
+			control['verbose'] = True
+			print("Verbose mode is not yet implemented")
+		elif opt in ('-v'):
+			print("Version " + str(control['version']) + " of makeIt by Charles Becker")
+		elif opt in ('-h', '--help'):
+			print_help()
+		elif opt in ('-m'):
+			w = dict()
+			w = makeit(arg)
+			control['filename'] = w['filename']
+			control['ext'] = w['ext']
+			print(arg)
+			do_it = True
+		elif opt in ('-n'):
+			control['project'] = arg
+	if(do_it == True):
+		create(control)
+	else:
+		print("No file/project name was provided.")
 
 
-helper = Helper.Helper(project_name, file_name, True) 
-# Note: this prototype defaults to true
+options, remainder = getopt.getopt(sys.argv[1:], 'hm:vn:o:', ['Verbose',
+                                                              'version',
+                                                              'help'
+                                                              ])
 
-helper.getPrecompile()
-helper.makeProject()
-helper.writeFile()
+control_array = dict()
+control_array['override'] = False
+control_array['verbose'] = False
+control_array['version'] = '1.0'
+control_array['project'] = None
+
+
+if (__name__ == "__main__"):
+	main(sys.argv[1:], control_array)
