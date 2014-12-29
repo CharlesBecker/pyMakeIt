3ed3e82d34220543b1554c0113471eb505f23582
Charles Becker <charleshbecker@gmail.com>
2014-12-29 03:49:52 -0700

    Update makeIt.py
    second version of prototype, can be called from command line but does not yet accept arguments.  Not compatible with previous version of Helper.py module!!!!!!!

--- makeIt.py
+++ makeIt.py
@@ -1,9 +1,19 @@
+#!/usr/local/bin/python3 
+# change the #! to the proper version for your system
+# also make the file executable
+
+import getopt
+import sys
 import Helper
 
-file_name = "secondThing"
+project_name = "firstThing"
+file = "thirdThing"
 ext = "cpp"
+file_name = {'filename': file, 'ext': ext}
 
-helper = Helper.Helper(file_name, ext)
+
+helper = Helper.Helper(project_name, file_name, True) 
+# Note: this prototype defaults to true
 
 helper.getPrecompile()
 helper.makeProject()
